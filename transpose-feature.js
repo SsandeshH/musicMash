470bad47f834c65b2942ec1b4df69dc4c8242f4a
San <ssandeshupretii@gmail.com>
2025-03-08 22:33:43 +0545

    Transpose feature,before minor addition - Sandesh

--- transpose-feature.js
+++ transpose-feature.js
@@ -4,42 +4,85 @@
 $(function(){
 
     const chords = {
-        'c' : 1,
-        'c#' : 2,
-        'd' : 3,
-        'd#' : 4,
-        'e' : 5,
-        'f' : 6,
-        'g' : 7,
-        'g#' : 8,
-        'a' : 9,
-        'a#' : 10,
-        'b' : 11
+        'C' : 1,
+        'C#' : 2,
+        'D' : 3,
+        'D#' : 4,
+        'E' : 5,
+        'F' : 6,
+        'F#' : 7,
+        'G' : 8,
+        'G#' : 9,
+        'A' : 10,
+        'A#' : 11,
+        'B' : 12
     }
 
+    
+    // Box Animation
     $(".transpose").on("click",function(){
         $(".menu-box").animate({ top: '10vh'})
         $(".features").show(500).css({
                 display : "flex",
                 justifyContent : "space-around"
             })
-        })
+    })
 
-        $(".add").on("click",function(){
+
+    //add
+    $(".add").on("click",function(){
             values = $(".chords-inp").val()
             if(values != '' || values.trim() != ''){
-                let inp_list = values.split(" ").map(item => item.toLowerCase())
-                // inp_list = inp_list.toLowerCase()
-                
-                for(let key in inp_list){
-
-                }
-                
+                let inp_list = values.split(" ").map(item => item.toUpperCase())
+                console.log(inp_list)
+                let results = []
+                inp_list.forEach(element => {
+                    let val = (chords[element])
+                    val += 1
+                    if(val % 12 == 0){
+                        val = 1
+                        results.push(Object.keys(chords).find(value => chords[value] == val%12))
+                        $(".chords-inp").val(results.join(" "));
+                    }else{
+                        results.push(Object.keys(chords).find(value => chords[value] == val%12))
+                        $(".chords-inp").val(results.join(" "));
+                    }
+                    
+                });
+                    
             }
             else{
-                console.log('retry')
+                $(".chords-inp").val("Retry");
             }
-        })
-        
+    })
+
+
+    // Subtract
+    $(".substract").on("click", function() {
+        values = $(".chords-inp").val();
+        if (values != '' || values.trim() != '') {
+            let inp_list = values.split(" ").map(item => item.toUpperCase());
+            let results = [];
+
+            inp_list.forEach(element => {
+                let val = chords[element];
+                val -= 1;
+                if (val == 0) {
+                    val = 12;
+                    results.push(Object.keys(chords).find(value => chords[value] == val));
+                } else {
+                    results.push(Object.keys(chords).find(value => chords[value] == val % 12));
+                }
+            });
+
+            $(".chords-inp").val(results.join(" "));
+
+        } else {
+            $(".chords-inp").val("Retry");
+        }
+    });
+            
 })
+        
+
 
