660a80aa20e28854561c415f8b7bda6ba0ae8419
San <ssandeshupretii@gmail.com>
2025-03-12 00:58:42 +0545

    Styles and Minor check function add-sandesh

--- index-js.js
+++ index-js.js
@@ -1,13 +1,4 @@
-//feature for transpose using js and jquery
-
-//select the button transpose
 $(function(){
-
-    const chords = {
-        'C' : 1, 'C#' : 2, 'D' : 3, 'D#' : 4, 'E' : 5,'F' : 6,
-        'F#' : 7, 'G' : 8, 'G#' : 9, 'A' : 10, 'A#' : 11,'B' : 12
-    }
-    
     // Box Animation
     $(".transpose").on("click",function(){
         $(".menu-box").animate({ top: '10vh'})
@@ -48,16 +39,16 @@ $(function(){
                         "outline": ""
                     });
             });
-
             $(".features").css({
                 "background-color" : "rgb(49, 47, 47)",
                 "box-shadow": "rgba(232, 229, 232, 0.25) 0px 50px 100px -20px, rgba(255, 255, 255, 0.3) 0px 30px 60px -30px, rgba(232, 229, 232, 0.35) 0px -2px 6px 0px inset"
 
             })
-            
-            $(".sun").show()
+
+            $(".sun").show("fast")
         })
     })
+
     $(".sun").on("click",function(){
         $(".sun").hide("fast",function(){
             $("body").css({
@@ -68,9 +59,7 @@ $(function(){
                 "background-color" : "rgb(232, 229, 232)",
                 "box-shadow": "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset"
             })
-            $("button").css({
-                "color" : "white"
-            })
+           
             $("button").css({
                 "background-color" : "rgb(232, 229, 232)",
                 "color" : "black"
@@ -91,90 +80,16 @@ $(function(){
                         "outline": ""
                     });
                 });
-        })
-        $(".features").css({
-            "background-color" : "rgb(232, 229, 232)",
-            "box-shadow": "rgba(232, 229, 232, 0.25) 0px 50px 100px -20px, rgba(255, 255, 255, 0.3) 0px 30px 60px -30px, rgba(232, 229, 232, 0.35) 0px -2px 6px 0px inset"
+            $(".features").css({
+                "background-color" : "rgb(232, 229, 232)",
+                "box-shadow": "rgba(232, 229, 232, 0.25) 0px 50px 100px -20px, rgba(255, 255, 255, 0.3) 0px 30px 60px -30px, rgba(232, 229, 232, 0.35) 0px -2px 6px 0px inset"
+        
+            })
 
+                
         })
+        
+
         $(".moon").show("fast")
     })
-
-    function chord_check(inp_list) {
-        let check_list = [];
-        
-        for (let i = 0; i < inp_list.length; i++) {
-            const letters = inp_list[i].split('');
-            if (letters[letters.length - 1] == 'M') {
-                check_list.push(1);  // Minor chord
-            } else {
-                check_list.push(0);  // Major chord
-            }
-        }
-        console.log(check_list);
-        return check_list;
-    }
-    
-
-
-    //add
-    $(".add").on("click",function(){
-            values = $(".chords-inp").val()
-            if(values != '' || values.trim() != ''){
-                let inp_list = values.split(" ").map(item => item.toUpperCase())
-                console.log(inp_list)
-
-                check_list = chord_check(inp_list)
-                let results = []
-
-                inp_list.forEach(element => {
-                    let val = (chords[element])
-                    val += 1
-                    if(val % 12 == 0){
-                        val = 1
-                        results.push(Object.keys(chords).find(value => chords[value] == val%12))
-                        
-                    }else{
-                        results.push(Object.keys(chords).find(value => chords[value] == val%12))
-                        
-                    }
-                    $(".chords-inp").val(results.join(" "));
-                    
-                });
-                    
-            }
-            else{
-                $(".chords-inp").val("Retry");
-            }
-    })
-
-
-    // Subtract
-    $(".substract").on("click", function() {
-        values = $(".chords-inp").val();
-        if (values != '' || values.trim() != '') {
-            let inp_list = values.split(" ").map(item => item.toUpperCase());
-            let results = [];
-
-            inp_list.forEach(element => {
-                let val = chords[element];
-                val -= 1;
-                if (val == 0) {
-                    val = 12;
-                    results.push(Object.keys(chords).find(value => chords[value] == val));
-                } else {
-                    results.push(Object.keys(chords).find(value => chords[value] == val % 12));
-                }
-            });
-
-            $(".chords-inp").val(results.join(" "));
-
-        } else {
-            $(".chords-inp").val("Retry");
-        }
-    });
-            
-})
-        
-
-
+}) 
---
+++ transposin-feature.js
@@ -0,0 +1,68 @@
+$(function(){
+
+    //chord map
+    const chords = {
+        'C' : 1, 'C#' : 2, 'D' : 3, 'D#' : 4, 'E' : 5,'F' : 6,
+        'F#' : 7, 'G' : 8, 'G#' : 9, 'A' : 10, 'A#' : 11,'B' : 12
+    }
+
+    //check chords
+    function check_chords(inpList){
+        return inpList.map(element => element.endsWith('M')?1:0)
+    }
+
+    function extract_notes(inpList){
+        return inpList.map(element => element.replace('M',''))
+    }
+
+    //add logic
+    $(".add").on("click",function(){
+        if($(".chords-inp").val()!= "" || $(".chords-inp").val().trim() != ""){
+            let inpList = $(".chords-inp").val().toUpperCase().split(' ')
+            const checkList = check_chords(inpList)
+
+            let processedList = extract_notes(inpList)
+
+            let newChords = []
+            for(let i = 0;i<inpList.length;i++){
+            let key = chords[processedList[i]]
+            key ++;
+            if(key > 12){
+                key = 1
+            }
+            newChords[i] = Object.keys(chords).find(chord => chords[chord] == key)
+
+            if(checkList[i]==1) newChords[i] += 'm'
+            $(".chords-inp").val(newChords.join(' '))
+        }
+    }else{
+            $(".chords-inp").val("Retry")
+    }  
+    })
+
+    $(".substract").on("click",function(){
+        if($(".chords-inp").val()!= "" || $(".chords-inp").val().trim() != ""){
+        let inpList = $(".chords-inp").val().toUpperCase().split(' ')
+        
+        const checkList = check_chords(inpList)
+
+        let processedList = extract_notes(inpList)
+
+        let newChords = []
+        for(let i = 0;i<inpList.length;i++){
+           let key = chords[processedList[i]]
+           key --;
+        if(key <= 0){
+            key = 12
+        }
+        newChords[i] = Object.keys(chords).find(chord => chords[chord] == key)
+
+        if(checkList[i]==1) newChords[i] += 'm'
+        }
+
+        $(".chords-inp").val(newChords.join(' '))
+    }else{
+        $(".chords-inp").val("Retry")
+    }
+    })
+})
