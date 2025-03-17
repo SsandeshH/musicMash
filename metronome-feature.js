00c5eccd1f613209d59437200ad62770d68ce204
San <ssandeshupretii@gmail.com>
2025-03-17 23:36:37 +0545

    Added Tap Feature on Metronome -Sandesh

--- metronome-feature.js
+++ metronome-feature.js
@@ -1,5 +1,5 @@
 $(function(){
-    const note2 = new Audio("/mp3/low.mp3")
+    const note2 = new Audio("mp3/low.mp3")
     let intervals = null
     let state = 0
     $(".play").on("click",function(){
@@ -9,12 +9,30 @@ $(function(){
             $(".play").text("Stop")
             clearInterval(intervals)
             intervals = setInterval(function(){
-            note2.cloneNode().play()
+                note2.cloneNode().play()
             },Math.round(60000/bpm))
         }else{
             $(".play").text("Play")
             clearInterval(intervals)
             note2.pause()
+            note2.currentTime = 0;
         }
     })
+
+
+    let taps = []
+    $(".tap").on("click", function(event) {
+        let now = performance.now();
+        inpLag =now - event.timeStamp;
+        console.log(now)
+        if (taps.length > 0) {
+            
+            let taplength = now - taps[taps.length - 1];
+            let newBpm = Math.round((60000 / taplength) - inpLag);
+            $(".bpm").val(newBpm)
+        }
+        taps.push(now);
+    });
+    
+    
 })
