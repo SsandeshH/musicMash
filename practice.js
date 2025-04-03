d0e2cdc19d3fdc846d55c02e368cc097b8551a1e
Noiceinmydrink <eesangeree@gmail.com>
2025-04-03 12:05:46 +0545

    Modified metronome UI

---
+++ practice.js
@@ -0,0 +1,4 @@
+const tempoDisplay = document.querySelector('.js-tempo');
+const decreaseTempoBtn = document.querySelector('.js-decrease-bpm');
+const increaseTempoBtn = document.querySelector('.js-increase-bpm');
+const startStopBtn = document.querySelector('.js-start-stop');
